b45b58c6b7e38cb3d6e088069e6827870e87a2b9
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-05-16 23:19:01 -0300

    adding works page

---
+++ assets/javascripts/theme.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	var plusClass = ".plus";
+	var minusClass = ".minus";
+
+	$(plusClass).on("click",function(){
+		var self = $(this);
+		self.siblings('.text-more').show()
+		self.hide()
+		$(minusClass).show();
+	});
+
+	$(minusClass).on("click",function(){
+		var self = $(this);
+		self.siblings('.text-more').hide()
+		self.hide()
+		$(plusClass).show();
+	});
+})
